0d01feac98bf5fae3c80b36387ab8f8bba872268
umer khan <umer.kdev@gmail.com>
2019-02-20 18:08:46 +0200

    adding todo component

--- src/App.js
+++ src/App.js
@@ -1,13 +1,32 @@
-import React, { Component } from 'react';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
+import Todos from "./components/Todos";
 
 class App extends Component {
+  state = {
+    todos: [
+      {
+        id: 1,
+        title: "Just Do It",
+        isCompleted: false
+      },
+      {
+        id: 2,
+        title: "bring it on baby",
+        isCompleted: false
+      },
+      {
+        id: 3,
+        title: "wanna hang out tonight at my plave",
+        isCompleted: true
+      }
+    ]
+  };
+
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-        
-        </header>
+        <Todos todos={this.state.todos} />
       </div>
     );
   }
