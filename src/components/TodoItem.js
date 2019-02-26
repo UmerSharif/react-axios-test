6f076be3173b77c79b1ce32f1af7d98a05c73225
Umer Khan <umer.kdev@gmail.com>
2019-02-26 23:57:43 +0200

    styling p in todoitem comp

--- src/components/TodoItem.js
+++ src/components/TodoItem.js
@@ -1,11 +1,15 @@
 import React, { Component } from 'react'
 import PropTypes from 'prop-types'
 
+const textC = {
+  color: '#dfe1e4'
+  
+}
 export class TodoItem extends Component {
 
     getStyle = () => {
         return {
-            background: '#f4f4f4',
+            background: '#036564',
             padding: '10px',
             borderBottom: '1px #ccc dotted',
             textDecoration: this.props.todo.isCompleted ? 'line-through': 'none'
@@ -18,11 +22,13 @@ export class TodoItem extends Component {
       }
     }
 
+
+
   render() {
       const { id , title} = this.props.todo
     return (
       <div style={this.getStyle()}>
-        <p>
+        <p style={textC}>
            <input type="checkbox" onChange={this.props.checkMark.bind(this, id)}/>
            {title}
            <button onClick={this.props.deleteNode.bind(this,id)} style={this.buttonStyle()}>Delete</button>
