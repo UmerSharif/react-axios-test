c8213262cf31f057a6fbfd5eb04e960be6fb7dca
Umer Khan <umer.kdev@gmail.com>
2019-02-26 23:55:42 +0200

    styling header comp

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,8 +2,20 @@ import React from 'react'
 
 export default function Header() {
   return (
-    <div>
-      
-    </div>
+    <header style={headerStyle}>
+      <h1 style={textC}>React CRUD</h1>
+    </header>
   )
+}
+
+
+
+const headerStyle =  {
+      background: '#335159',
+      padding: '10px'   
+}
+
+const textC = {
+    color: '#dfe1e4'
+    
 }
