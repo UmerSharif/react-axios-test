ac438dae54b81914cbcc0e809587d5808b3c564c
Umer Khan <umer.kdev@gmail.com>
2019-03-05 23:52:27 +0200

    adding state to form

--- src/components/AddTodo.js
+++ src/components/AddTodo.js
@@ -1,11 +1,27 @@
 import React, { Component } from 'react'
 
 export default class AddTodo extends Component {
+  state = {
+      title : ''
+  }
+
+  onChange = (e) =>{
+    return this.setState({title: e.target.value})
+  }
   render() {
     return (
       <form style={{display: 'flex'}}>
-      <input style={{flex: '10', padding: '10px'}} type="text" name="title" placeholder="Enter Here...."/>
-      <input style={{flex: '1', padding:'10px'}} type="submit" value="Submit"/>
+      <input style={{flex: '10', padding: '10px'}} 
+      type="text" 
+      name="title" 
+      placeholder="Enter Here...."
+      value={this.state.title}
+      onChange={this.onChange}
+      />
+
+      <input style={{flex: '1', padding:'10px'}}
+       type="submit" 
+       value="Submit"/>
       </form>
     )
   }
